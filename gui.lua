aeafd3ea184d419301c73c24c4c38b730a6ee349
Christopher Moore <christopher.e.moore@gmail.com>
2026-02-05 19:48:15 +0700

    more

--- gui.lua
+++ gui.lua
@@ -443,6 +443,7 @@ args:
 	mouse - (optional) - if mouse isn't provided then one will be created, and it'll be updated during GUI:update
 --]]
 function GUI:init(args)
+	args = args or {}
 	self.drawImmediateMode = args.drawImmediateMode
 	if not self.drawImmediateMode then
 		self.view = require 'glapp.view'()
